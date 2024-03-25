673b040e1aa4cabdccbf58c40b1e98538ec13cb3
yeonghyeon <agilecatch@gmail.com>
2024-03-25 19:35:32 +0900

    [New] start new project

---
+++ app/src/main/java/com/agileatch/retrofit_video/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agileatch.retrofit_video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
