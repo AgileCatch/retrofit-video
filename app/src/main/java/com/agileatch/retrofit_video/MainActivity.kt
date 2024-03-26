4c20da24d6b50bdd25d87559286def2358e8641a
yeonghyeon <agilecatch@gmail.com>
2024-03-26 22:32:28 +0900

    [New] viewBinding설정

--- app/src/main/java/com/agileatch/retrofit_video/MainActivity.kt
+++ app/src/main/java/com/agileatch/retrofit_video/MainActivity.kt
@@ -2,10 +2,19 @@ package com.agileatch.retrofit_video
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.agileatch.retrofit_video.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView(){
+        TODO("Not yet implemented")
     }
 }
